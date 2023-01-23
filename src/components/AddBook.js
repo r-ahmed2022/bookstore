a58cd36c96cc799dc256b8bdca0b2310edbe2f56
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-23 16:16:00 +0530

    updated addbook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/books';
+import { useDispatch, useSelector } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { ADD_BOOK } from '../redux/books/books';
 
 export default function AddBook() {
+  const { books } = useSelector((state) => state.bookList);
   const dispatch = useDispatch();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newBook = {
+      ...books,
+      id: uuidv4(),
+      title: e.target[0].value,
+      author: e.target[1].value,
+    };
+    dispatch(ADD_BOOK(newBook));
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+  };
   return (
-    <form className="addbook-form" onSubmit={() => dispatch(addBook)}>
+    <form
+      className="addbook-form"
+      onSubmit={handleSubmit}
+    >
       <input
         type="text"
         maxLength={20}
-        id="addbook"
+        id="title"
+        name="title"
         placeholder="Book title"
         required
       />
@@ -17,9 +35,10 @@ export default function AddBook() {
       <input
         type="text"
         maxLength={20}
-        id="author-input"
+        id="author"
         placeholder="Author name"
         required
+        name="author"
       />
 
       <button className="add-btn" type="submit">
