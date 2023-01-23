9ba4a196b771c0f26210a0fa3b068fd706bf44c2
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-23 17:17:10 +0530

    updated Books and Booklist.js

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { REMOVE_BOOK } from '../redux/books/books';
 
-export default function Book(props) {
-  const { category, title, author } = props;
+function Book(props) {
+  const { id, title, author } = props;
+  const dispatch = useDispatch();
   return (
     <div className="book-section">
       <div className="book-info">
         <h6 className="category grey-text">
-          {category}
+          {}
         </h6>
         <h2 className="book-title">{title}</h2>
         <span className="author-info">{author}</span>
         <ul className="actions">
-          <li className="action-link blue-text">Comments</li>
-          <li className="action-link blue-text">Remove</li>
-          <li className="action-link blue-text">Edit</li>
+          <li className="action-link blue-text"><input type="button" className=" btn blue-text" value="Comments" /></li>
+          <li className="action-link blue-text"><input type="button" className="btn blue-text" value="Remove" onClick={() => dispatch(REMOVE_BOOK(id))} /></li>
+          <li className="action-link blue-text"><input type="button" className="btn blue-text" value="Edit" /></li>
         </ul>
       </div>
     </div>
   );
 }
+
+export default Book;
--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -1,14 +1,10 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable max-len */
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
 export default function BooksList() {
   const { books } = useSelector((state) => state.bookList);
-  const dispatch = useDispatch();
   const booklist = books.map((book) => <Book key={book.id} {...book} />);
   return (
     <div>
