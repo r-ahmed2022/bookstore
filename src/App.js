ab8fb2a4b55505dcb16efadc0c85f77bc2520e1c
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-23 14:38:26 +0530

    updated index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-/* eslint-disable react/self-closing-comp */
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Books from './components/BooksList';
@@ -14,8 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="*" element={<Books />} />
-
       </Routes>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import store from './redux/configureStore';
 import './index.css';
 import App from './App';
-import store from './redux/configureStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
