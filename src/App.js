697fb9a2c35af290036c8414e3e34455623c0cf2
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-23 16:34:35 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Books from './components/BooksList';
+import AddBook from './components/AddBook';
 import Categories from './components/Categories';
 import './App.css';
 
@@ -10,8 +11,10 @@ function App() {
     <div className="container">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Books />} />
+        <Route exact path="/" element={<Books />} />
         <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<AddBook />} />
+
       </Routes>
     </div>
   );
