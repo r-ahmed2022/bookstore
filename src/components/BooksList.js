25d17d892cf350919d2933ad8ec25239e1bf5717
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-25 15:48:17 +0530

    updated Bookslist.js

--- src/components/BooksList.js
+++ src/components/BooksList.js
@@ -3,13 +3,23 @@ import { useSelector } from 'react-redux';
 import Book from './Book';
 import AddBook from './AddBook';
 
-export default function BooksList() {
-  const { books } = useSelector((state) => state.bookList);
-  const booklist = books.map((book) => <Book key={book.id} {...book} />);
+function BooksList() {
+  const { list } = useSelector((state) => state.booklist);
+  if (!list) {
+    return <h1>Loading books....</h1>;
+  }
+  const newList = Object.entries(list).map((data) => (
+    <Book
+      key={data[0]}
+      item_id={data[0]}
+      {...data[1][0]}
+    />
+  ));
   return (
-    <div>
-      {booklist}
+    <div className="bookscontainer">
+      { newList }
       <AddBook />
     </div>
   );
 }
+export default BooksList;
