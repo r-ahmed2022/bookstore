c75eb1dd80ecb179250bc7999b953ccbebf2fc7b
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-01-25 15:44:04 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { LIST_DATA } from './redux/books/showBookList';
 import Navbar from './components/Navbar';
 import Books from './components/BooksList';
 import Categories from './components/Categories';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(LIST_DATA());
+  }, [dispatch]);
   return (
     <div className="container">
-      <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Books />} />
-        <Route path="*" element={<Books />} />
-        <Route path="/categories" element={<Categories />} />
-      </Routes>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Books />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
